911de403b7e0f8bc1d486fde879fc2cc52ef9fc8
xyh <xyaohong@gmail.com>
2016-06-14 15:56:39 +0800

    fixed untracked files
    Former-commit-id: 918eec3716d95b948e0bc85857b76e024c620075
    Former-commit-id: ad8957946149ac675d53d04a1fb9380efe0056f3

---
+++ Consumer/Consumer.h
@@ -0,0 +1,51 @@
+#include <pthread.h>
+#include <ndn-cpp/face.hpp>
+#include "util/CircularQueue.h"
+#include "utils.h"
+#include "object.h"
+#include "face-wrapper.h"
+
+using namespace std;
+using namespace ndn;
+using namespace ndn::func_lib;
+
+
+#define WIDTH 640
+#define HEIGHT 480
+
+
+struct frame_buf
+{
+	size_t size;
+	uint8_t p_In_Frame[WIDTH * HEIGHT * 3 / 2];
+};
+
+
+class Consumer
+{
+public:
+	Consumer ( boost::shared_ptr<FaceWrapper> faceWrapper );
+
+
+	~Consumer ();
+
+
+	void onData(const ptr_lib::shared_ptr<const Interest>& interest, const ptr_lib::shared_ptr<Data>& data);
+	
+
+	void onTimeout(const ptr_lib::shared_ptr<const Interest>& interest);
+	
+
+	int callbackCount_;
+
+	boost::shared_ptr<FaceWrapper> faceWrapper_;
+
+
+private:
+
+	FILE *pf;
+	
+	CirQueue<frame_buf> *recv_buf;
+	pthread_mutex_t recv_buf_mutex;
+};
+
